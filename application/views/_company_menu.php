1ede451373f9798f574ab5e1ddcab6cf6b90cb2a
Leonardo Cesca <cesca.leonardo@gmail.com>
2021-04-19 18:12:52 +0200

    homepage fashion

--- application/views/_company_menu.php
+++ application/views/_company_menu.php
@@ -1,14 +1,14 @@
 <div id="mainMenuBackdrop">
 	<div id="mainMenu">
-		<a href="<?= site_url() ?>orders/index/company/<?= _COMPANY_URI ?>" class="parent-to-be-hovered main-link">
-			<div class="d-flex link-block">
-				<div><i class="mdi mdi-format-list-bulleted-square"></i> Ordini</div>
-				<div class="ml-auto show-on-parent-hover" desktop><i class="mdi mdi-chevron-right"></i></div>
-			</div>
-		</a>
 		<a href="<?= site_url() ?>main/index" class="parent-to-be-hovered main-link">
 			<div class="d-flex link-block">
 				<div><i class="mdi mdi-home"></i> Home</div>
+				<div class="ml-auto show-on-parent-hover" desktop><i class="mdi mdi-chevron-right"></i></div>
+			</div>
+		</a>
+		<a href="<?= site_url() ?>orders/index/company/<?= _COMPANY_URI ?>" class="parent-to-be-hovered main-link">
+			<div class="d-flex link-block">
+				<div><i class="mdi mdi-format-list-bulleted-square"></i> Ordini</div>
 				<div class="ml-auto show-on-parent-hover" desktop><i class="mdi mdi-chevron-right"></i></div>
 			</div>
 		</a>
--- application/views/home.php
+++ application/views/home.php
@@ -2,29 +2,37 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
-<?php html_head('Home') ?>
+<?php html_head('Home', ['home']) ?>
 <body>
 	<?php topbar() ?>
 	<?php main_menu() ?>
 	<div id="pageContainer">
-		<div style="padding: 16px;">
-			<div class="flex-wrap">
-				<div class="card">
-					<div class="general-heading">Informazioni</div>
-					<div>
-						email: <?= $this->session->user['email'] ?>
-					</div>
+		<div class="flex-wrap">
+			<div class="reducer">
+				<div id="splash">
+					<div class="title">PonyManager, Gestione semplice ed efficace</div>
+					<div class="subtitle">L'unico tool online per facilitare la gestione degli ordini. Ottimizzato per gli esercizi che effettuano consegne a domicilio grazie all'integrazione con Google Maps</div>
 				</div>
-				<?php foreach ($privileges as $privilege) { ?>
-					<a class="card" href="<?= site_url() ?>orders/index/company/<?= $privilege['uri_name'] ?>" style="color: black;">
-						<div class="general-heading"><?= $privilege['name'] ?></div>
-						<?php if (_GLOBAL_USER['email'] == $privilege['owner']) { ?>
-							<div>Privilegi da proprietario</div>
-						<?php } else { ?>
-							<div>Accesso limitato</div>
-						<?php } ?>
-					</a>
-				<?php } ?>
+			</div>
+			<div class="fixer">
+				<div class="flex-wrap">
+					<div class="card">
+						<div class="general-heading">Informazioni</div>
+						<div>
+							email: <?= $this->session->user['email'] ?>
+						</div>
+					</div>
+					<?php foreach ($privileges as $privilege) { ?>
+						<a class="card" href="<?= site_url() ?>orders/index/company/<?= $privilege['uri_name'] ?>" style="color: black;">
+							<div class="general-heading"><?= $privilege['name'] ?></div>
+							<?php if (_GLOBAL_USER['email'] == $privilege['owner']) { ?>
+								<div>Privilegi da proprietario</div>
+							<?php } else { ?>
+								<div>Accesso limitato</div>
+							<?php } ?>
+						</a>
+					<?php } ?>
+				</div>
 			</div>
 		</div>
 	</div>
